7744eb04264e19bbf9888f7c68e9769ea6f1687c
Enzo DO ROSARIO <contact@edorosario.fr>
2024-04-21 20:26:02 +0200

    make it work

--- src/lib/slices/alerts.slice.ts
+++ src/lib/slices/alerts.slice.ts
@@ -1,11 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { alertsAdapter } from "../models/alert.entity";
 import { RootState } from "../create-store";
+import { addToFavorites } from "../usecases/add-to-favorites.usecase";
 
 export const alertsSlice = createSlice({
   name: "alerts",
   initialState: alertsAdapter.getInitialState(),
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(addToFavorites.fulfilled, (state, action) => {
+      alertsAdapter.addOne(state, {
+        id: alertsAdapter.getSelectors().selectIds(state).length.toString(),
+        message: `${action.payload.name} a été ajouté à vos favoris.`,
+      });
+    });
+  },
 });
 
 export const selectLastAlert = (state: RootState) =>
